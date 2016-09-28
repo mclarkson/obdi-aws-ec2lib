2bf4015ea8c7e96651955f63395c65e75c8aec7e
Mark Clarkson <mark.clarkson@smorg.co.uk>
2016-09-28 09:41:14 +0100

    tidy

--- go/scraps/register_image.go
+++ go/scraps/register_image.go
@@ -1,5 +1,9 @@
 package main
 
+// go build register_image.go
+// export AWS_ACCESS_KEY_ID=AKIAXXXXXXXEEEEEEB2Q
+// export AWS_SECRET_ACCESS_KEY=wTqsdfiuHSOoihfaHOIWOIFhaosifhOAIHFoaiaS
+
 import (
 	"fmt"
 
@@ -15,36 +19,53 @@ func main() {
 
 	svc := ec2.New(session.New(), &aws.Config{Region: aws.String(region)})
 
-	bdm := ec2.BlockDeviceMapping{
-		DeviceName: aws.String("sda1"),
-		Ebs: &ec2.EbsBlockDevice{
-			DeleteOnTermination: aws.Bool(true),
-			//Encrypted:           aws.Bool(false),
-			//Iops:                aws.Int64(1),
-			SnapshotId: aws.String("snap-eb8c4617"),
-			VolumeSize: aws.Int64(21),
-			VolumeType: aws.String("gp2"),
-		},
-		//NoDevice:    aws.String("String"),
-		//VirtualName: aws.String("String"),
-	}
+	/*
+		bdm := ec2.BlockDeviceMapping{
+			DeviceName: aws.String("sda1"),
+			Ebs: &ec2.EbsBlockDevice{
+				DeleteOnTermination: aws.Bool(true),
+				//Encrypted:           aws.Bool(false),
+				//Iops:                aws.Int64(1),
+				SnapshotId: aws.String("snap-eb8c4617"),
+				VolumeSize: aws.Int64(21),
+				VolumeType: aws.String("gp2"),
+			},
+			//NoDevice:    aws.String("String"),
+			//VirtualName: aws.String("String"),
+		}
 
-	var bdms []*ec2.BlockDeviceMapping
-	bdms = append(bdms, &bdm)
+		var bdms []*ec2.BlockDeviceMapping
+		bdms = append(bdms, &bdm)
+	*/
 
 	params := &ec2.RegisterImageInput{
-		Name: aws.String("PRET-TEST-NAME"), // Required
-		//Architecture: aws.String("ArchitectureValues"),
-		BlockDeviceMappings: bdms,
-		Description:         aws.String("PRET-TEST-DESC"),
-		DryRun:              aws.Bool(false),
+		Name: aws.String("PRET-TEST-NAME"), // AMI Name
+		BlockDeviceMappings: []*ec2.BlockDeviceMapping{
+			&ec2.BlockDeviceMapping{
+				DeviceName: aws.String("sda1"),
+				Ebs: &ec2.EbsBlockDevice{
+					DeleteOnTermination: aws.Bool(true),
+					SnapshotId:          aws.String("snap-eb8c4617"),
+					VolumeSize:          aws.Int64(21),
+					VolumeType:          aws.String("gp2"),
+					//Encrypted:           aws.Bool(false),
+					//Iops:                aws.Int64(1),
+				},
+				//Architecture: aws.String("ArchitectureValues"),
+				//NoDevice:    aws.String("String"),
+				//VirtualName: aws.String("String"),
+			},
+		},
+
+		Description:        aws.String("PRET-TEST-DESC"),
+		DryRun:             aws.Bool(false),
+		RootDeviceName:     aws.String("sda1"),
+		VirtualizationType: aws.String("hvm"),
 		//EnaSupport:         aws.Bool(true),
 		//ImageLocation:      aws.String("String"),
 		//KernelId:           aws.String("String"),
 		//RamdiskId:          aws.String("String"),
-		RootDeviceName: aws.String("sda1"),
 		//SriovNetSupport:    aws.String("String"),
-		VirtualizationType: aws.String("hvm"),
 	}
 
 	resp, err := svc.RegisterImage(params)
